db7ae1ce33720b5ee06a30b505e86d7f95c50703
Erodot0 <janmanpreet.singh@gmail.com>
2025-02-21 16:18:37 +0100

    removed duplicate type declaration for user and session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,11 +1,17 @@
-import NextAuth, { DefaultSession } from 'next-auth';
+import 'next-auth';
 
 declare module 'next-auth' {
-  type UserSession = DefaultSession['user'];
-  interface Session {
-    user: UserSession;
+  interface User {
+    id: string;
+    name?: string;
+    email?: string;
+    token?: string; // Add the `token` property
+    isAdmin?: boolean;
   }
 
+  interface Session {
+    user: User;
+  }
   interface CredentialsInputs {
     email: string;
     password: string;
